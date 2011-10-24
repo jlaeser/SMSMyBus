3b50c41beffb0a62a638f22128ffcfb5a122b07e
Greg Tracy <gtracy@gmail.com>
2011-10-24 15:10:13 -0500

    re-added some missing package imports

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ import os
 import wsgiref.handlers
 import logging
 
+from google.appengine.api import memcache
+
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp.util import run_wsgi_app
 
 from data_model import PhoneLog
-
+import config
 
 class MainHandler(webapp.RequestHandler):
 
