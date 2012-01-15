f120019156f80e0c879cda5e4ed7985f70711c1d
Greg Tracy <gtracy@gmail.com>
2012-01-14 20:25:52 -0600

    implemented a force refresh on the memcached/datastore backed getroutes call

--- api/v1/getroutes.py
+++ api/v1/getroutes.py
@@ -13,6 +13,7 @@ from google.appengine.ext import db
 from google.appengine.ext.webapp.util import run_wsgi_app
 
 from api.v1 import utils
+from data_model import RouteListing
 
 class StaticAPIs(db.Model):
     method = db.StringProperty()
@@ -25,7 +26,7 @@ class MainHandler(webapp.RequestHandler):
     def get(self):
       
       # validate the request parameters
-      devStoreKey = 'nomar'#validateRequest(self.request,utils.GETROUTES)
+      devStoreKey = validateRequest(self.request,utils.GETROUTES)
       if devStoreKey is None:
           logging.debug("unable to validate the request parameters")
           self.response.headers['Content-Type'] = 'application/javascript'
@@ -33,34 +34,36 @@ class MainHandler(webapp.RequestHandler):
           return
       
       logging.debug('getroutes request...  ')
+      if self.request.get('force') is not '':
+          refresh = True
+      else:
+          refresh = False
       
       if utils.afterHours() is True:
           # don't run these jobs during "off" hours
 	      json_response = utils.buildErrorResponse('-1','The Metro service is not currently running')
       else:
-          json_response = memcache.get(utils.GETROUTES)
-          if json_response is None:
-              api = db.GqlQuery('select * from StaticAPIs where method = :1', utils.GETROUTES).get()
-              if api is not None:
-                  logging.debug('---> datastore hit');
-                  json_response = api.json
-              else:
-                  json_response = getRoutes()
-                  static = StaticAPIs()
-                  static.method = utils.GETROUTES
-                  static.json = json_response
-                  static.put()
-                  
+          if refresh is True:
+              json_response = getRoutes(refresh)
+
               # drop it into the memcache again
-              memcache.add(utils.GETROUTES, json_response)
+              memcache.set(utils.GETROUTES, json_response)
               logging.debug('---> storing in memcache');
           else:
               logging.debug('---> memcache hit');
-              
+              json_response = memcache.get(utils.GETROUTES)
+              if json_response is None:
+                  json_response = getRoutes(refresh)
+        
+                  # drop it into the memcache again
+                  memcache.set(utils.GETROUTES, json_response)
+                  logging.debug('---> storing in memcache');
+                  
+
+          # record the API call for this devkey              
           utils.recordDeveloperRequest(devStoreKey,utils.GETROUTES,self.request.query_string,self.request.remote_addr);
 
       #logging.debug('API: json response %s' % json_response);    
-      # encapsulate response in json
       callback = self.request.get('callback')
       if callback is not '':
           self.response.headers['Content-Type'] = 'application/javascript'
@@ -81,26 +84,35 @@ class MainHandler(webapp.RequestHandler):
 ## end MainHandler
 
 
-def getRoutes():
-    
-    q = db.GqlQuery('select * from RouteListing order by route')
-    routes = q.fetch(2000)
-    if routes is None:
-        response_dict = {'status':'0',
-                         'info':'No routes found'
-                        }
-        return response_dict
-        
-    response_dict = {'status':'0',
-                    }    
-    ## stopped here trying to create a map of unique routes and endpoints
-    ##
-    hits = {}
-    for r in routes:
-        # are we tracking this route/direction pair?
-        key = r.route + ':' + r.direction
-        hits[key] = hits.get(key,0) + 1
+def getRoutes(refresh):
 
+    if refresh is False:
+        # do we already have it in the datastore?
+        api = db.GqlQuery('select * from StaticAPIs where method = :1', utils.GETROUTES).get()
+        if api is not None:
+            logging.debug('---> datastore hit');
+            return api.json
+
+    logging.debug('---> datastore lookup starting!')
+    offset = 0
+    q = RouteListing.all()
+    routes = q.fetch(1000)
+
+    hits = {}
+    response_dict = {'status':0}
+    while len(routes) > 0:
+        offset += len(routes)
+
+        ## stopped here trying to create a map of unique routes and endpoints
+        ##
+        for r in routes:
+            # are we tracking this route/direction pair?
+            key = r.route + ':' + r.direction
+            hits[key] = hits.get(key,0) + 1
+
+        # get more routes
+        routes = q.fetch(1000,offset)
+        
     routeMap = {}        
     for k,v in hits.iteritems():
         key = k.split(':')
@@ -121,8 +133,14 @@ def getRoutes():
     
     # add the populated route details to the response
     response_dict.update({'routes':route_results})
-        
-    return simplejson.dumps(response_dict)
+    json = simplejson.dumps(response_dict)
+    
+    static = StaticAPIs()
+    static.method = utils.GETROUTES
+    static.json = json
+    static.put()
+
+    return json
 
 ## end getRoutes()
 
